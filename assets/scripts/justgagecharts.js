e21d597beda9118602222f4580171c9193bf5213
Wemmerson Albuquerque <38667364+WemmersonAlb@users.noreply.github.com>
2024-10-03 16:37:49 -0300

    JustGage implementado

--- assets/scripts/justgagecharts.js
+++ assets/scripts/justgagecharts.js
@@ -1,23 +1,121 @@
+let mostradorTemperatura;
+let mostradorUmidadade;
+let mostradorPressao;
+let mostradorLuz;
+let mostradorGas;
+let mostradorAr;
+let mostradorVelocidadeVento;
+let mostradorVoltagem;
+let mostradorRpm;
+
 export function preencherJustGageCharts(data){
 
     //temperatura
     document.querySelector("#graficoTemperatura").innerHTML="";
-    const tempChart= new JustGage({
+    mostradorTemperatura= new JustGage({
         id: "graficoTemperatura",
         value: data.temperatura,
         min: 0,
         max: 50,
-        title: "Temperatura °C"      
+        title: "Temperatura °C",
+        refreshAnimation: true,
+        relativeGaugeSize: true      
     });
-
-    //temperatura
+    
+    //umidade
     document.querySelector("#graficoUmidade").innerHTML="";
-    const umiChart= new JustGage({
+    mostradorUmidadade = new JustGage({
         id: "graficoUmidade",
         value: data.umidade,
         min: 0,
         max: 100,
-        title: "Umidade"      
+        title: "Umidade %",
+        refreshAnimation: true,
+        relativeGaugeSize: true      
+    });
+
+    //pressão
+    document.querySelector("#graficoPressao").innerHTML="";
+    mostradorPressao= new JustGage({
+        id: "graficoPressao",
+        value: data.pressao,
+        min: 900,
+        max: 1100,
+        title: "Pressão hPa",
+        refreshAnimation: true,
+        relativeGaugeSize: true      
+    });
+
+    //luz
+    document.querySelector("#graficoLuz").innerHTML="";
+    mostradorLuz= new JustGage({
+        id: "graficoLuz",
+        value: data.luminosidade,
+        min: 0,
+        max: 100000,
+        title: "Luminosidade lux",
+        refreshAnimation: true,
+        relativeGaugeSize: true      
+    });
+
+    //co2
+    document.querySelector("#graficoGas").innerHTML="";
+    mostradorGas= new JustGage({
+        id: "graficoGas",
+        value: data.cO2,
+        min: 0,
+        max: 1000,
+        title: "CO2 ppm",
+        refreshAnimation: true,
+        relativeGaugeSize: true      
+    });
+
+    //qualidade do ar
+    document.querySelector("#graficoAr").innerHTML="";
+    mostradorAr= new JustGage({
+        id: "graficoAr",
+        value: data.qualidadeDoAr,
+        min: 0,
+        max: 500,
+        title: "Qualidade do Ar AQI",
+        refreshAnimation: true,
+        relativeGaugeSize: true      
+    });
+
+    //velocidade do vento
+    document.querySelector("#graficoVelocidadeDoVento").innerHTML="";
+    mostradorVelocidadeVento= new JustGage({
+        id: "graficoVelocidadeDoVento",
+        value: data.velocidadeDoVento,
+        min: 0,
+        max: 100,
+        title: "Velocidade Do Vento km/h",
+        refreshAnimation: true,
+        relativeGaugeSize: true      
+    });
+
+    //voltagem
+    document.querySelector("#graficoVoltagem").innerHTML="";
+    mostradorVoltagem= new JustGage({
+        id: "graficoVoltagem",
+        value: data.voltagem,
+        min: 0,
+        max: 240,
+        title: "Voltagem V",
+        refreshAnimation: true,
+        relativeGaugeSize: true      
+    });
+
+    //RPM
+    document.querySelector("#graficoRpm").innerHTML="";
+    mostradorRpm= new JustGage({
+        id: "graficoRpm",
+        value: data.rpm,
+        min: 0,
+        max: 10000,
+        title: "RPM",
+        refreshAnimation: true,
+        relativeGaugeSize: true      
     });
 
 }
--- assets/scripts/maps.js
+++ assets/scripts/maps.js
@@ -18,14 +18,11 @@ allReadPlaca()
     placas.forEach((element) => {
       let marker = L.marker([element.latitude, element.longitude])
         .addTo(map)
-        .on("click", async function (e) {
+        .on("click", async function () {
           const placaOutPutData = await findLatestDataByPlacaId(element.id);
-          //preencherGrafico(placaOutPutData);
-          preencherJustGageCharts(placaOutPutData);
           setTimeout(async () => {
             const placaOutPutData = await findLatestDataByPlacaId(element.id);
             preencherJustGageCharts(placaOutPutData);
-            //preencherGrafico(placaOutPutData);
           }, 60000);
           const mapBox = document.querySelector("#map");
           const puxador = document.querySelector("#puxador");
@@ -37,7 +34,11 @@ allReadPlaca()
           mapBox.style.width = "30%";
           charts.style.display = "flex";
           charts.style.width = "70%";
-          map.invalidateSize();
+          charts.style.opacity = "1";
+          //map.invalidateSize();
+          setTimeout(()=>{
+            preencherJustGageCharts(placaOutPutData)
+          },700);          
         });
       marker.bindPopup(`${element.latitude} ${element.longitude}`).openPopup();
     });
@@ -45,14 +46,26 @@ allReadPlaca()
   .catch((error) => console.error("Erro ao carregar placas:", error));
 
 document.querySelector("#puxador").addEventListener("click", () => {
+  //map.setView([-8.059694, -34.951917], 9);
   const mapBox = document.querySelector("#map");
   const puxador = document.querySelector("#puxador");
   const charts = document.querySelector("#charts");
-  const historico = document.querySelector("#historico");
+  const historico = document.querySelector("#linkHistorico");
+
+  document.querySelector("#graficoTemperatura").innerHTML="";
+  document.querySelector("#graficoUmidade").innerHTML="";
+  document.querySelector("#graficoPressao").innerHTML="";
+  document.querySelector("#graficoLuz").innerHTML="";
+  document.querySelector("#graficoGas").innerHTML="";
+  document.querySelector("#graficoAr").innerHTML="";
+  document.querySelector("#graficoVelocidadeDoVento").innerHTML="";
+  document.querySelector("#graficoVoltagem").innerHTML="";
+  document.querySelector("#graficoRpm").innerHTML="";
+
   puxador.style.display = "none";
   historico.style.display = "none";
   mapBox.style.width = "100%";
   charts.style.display = "none";
   charts.style.width = "0%";
-  map.invalidateSize();
+ // map.invalidateSize();
 });
