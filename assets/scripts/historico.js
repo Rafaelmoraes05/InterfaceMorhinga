9f01513dd84e1a862f3a9e96b98fd6998b5c59cb
Wemmerson Albuquerque <38667364+WemmersonAlb@users.noreply.github.com>
2024-10-04 18:03:44 -0300

    Dygraphs implementado
    Existem alguns bugs para serem resolvidos como a data início ser depois da data fim, e os gráficos não carregarem assim que clica em pesquisa, precisando recarregar os nodes da página (F12).

--- assets/scripts/historico.js
+++ assets/scripts/historico.js
@@ -14,29 +14,44 @@ document.querySelector("#formTemporal").addEventListener("click", async ()=>{
     console.log(startDateISO)
 
     const data = await findByPlacaIdAndDateRange(id, startDateISO, endDateISO);
-    const labels = data.map(item => item.dataHoraString);
-    let dados = data.map(item => item.temperatura);
-    gerarGrafico('charts', 'Temperatura', ' °C', labels, dados);
-    dados = data.map(item => item.umidade);
-    gerarGrafico('charts', 'Umidade', ' %', labels, dados);
-    dados = data.map(item => item.pressao);
-    gerarGrafico('charts', 'Pressão', ' mmHG', labels, dados);
-    dados = data.map(item => item.luminosidade);
-    gerarGrafico('charts', 'Luminosidade', ' lx', labels, dados);
-    dados = data.map(item => item.cO2);
-    gerarGrafico('charts', 'CO2', ' ppm', labels, dados);
-    dados = data.map(item => item.qualidadeDoAr);
-    gerarGrafico('charts', 'Qualidade do Ar', '', labels, dados);
-    dados = data.map(item => item.velocidadeDoVento);
-    gerarGrafico('charts', 'Velocidade do Vento', ' km/h', labels, dados);
-    dados = data.map(item => item.voltagem);
-    gerarGrafico('charts', 'Voltagem', ' V', labels, dados);
-    dados = data.map(item => item.rpm);
-    gerarGrafico('charts', 'RPM', '', labels, dados);
+
+
+    //let dado1 = data.map(item=>[item.dataHoraString, item.temperatura]);
+    let dados = data.map(item => {
+        let [time, date] = item.dataHoraString.split(' ');
+        let [dia, mes, ano] = date.split('/');
+
+        let dataFormatada = new Date(ano, mes-1, dia, ...time.split(':'));
+        return [dataFormatada, item.temperatura, item.pressao, item.luminosidade, item.cO2, item.qualidadeDoAr, item.velocidadeDoVento, item.voltagem, item.rpm]
+
+    });
+
+    gerarGraficoDygraph(dados);
+    
+    //const labels = data.map(item => item.dataHoraString);
+    //let dados = data.map(item => item.temperatura);
+    //gerarGrafico('charts', 'Temperatura', ' °C', labels, dados);
+    // dados = data.map(item => item.umidade);
+    // gerarGrafico('charts', 'Umidade', ' %', labels, dados);
+    // dados = data.map(item => item.pressao);
+    // gerarGrafico('charts', 'Pressão', ' mmHG', labels, dados);
+    // dados = data.map(item => item.luminosidade);
+    // gerarGrafico('charts', 'Luminosidade', ' lx', labels, dados);
+    // dados = data.map(item => item.cO2);
+    // gerarGrafico('charts', 'CO2', ' ppm', labels, dados);
+    // dados = data.map(item => item.qualidadeDoAr);
+    // gerarGrafico('charts', 'Qualidade do Ar', '', labels, dados);
+    // dados = data.map(item => item.velocidadeDoVento);
+    // gerarGrafico('charts', 'Velocidade do Vento', ' km/h', labels, dados);
+    // dados = data.map(item => item.voltagem);
+    // gerarGrafico('charts', 'Voltagem', ' V', labels, dados);
+    // dados = data.map(item => item.rpm);
+    // gerarGrafico('charts', 'RPM', '', labels, dados);
 });
 
-function gerarGrafico(div, tipoDado, unidade, labels, dados){
-    
+
+//charts js
+function gerarGrafico(div, tipoDado, unidade, labels, dados){   
 
     const ctx = document.createElement('canvas');
     document.getElementById(div).appendChild(ctx);
@@ -71,4 +86,33 @@ function gerarGrafico(div, tipoDado, unidade, labels, dados){
             }
         }
     });
+}
+
+
+//dygraph
+function gerarGraficoDygraph(dados){
+    const chart = document.querySelector('#charts');
+    const labels = [
+        "Temperatura °C",
+        "Pressão hPa",
+        "Luminosidade lux",
+        "CO2 ppm",
+        "Qualidade do Ar AQI",
+        "Velocidade Do Vento km/h",
+        "Voltagem V",
+        "RPM"
+    ]
+    for(let i = 1; i<dados[0].length;i++){
+        const div = document.createElement('div');
+        div.setAttribute('class', 'graficoDygraph');
+        const d = dados.map(item=>[item[0], item[i]]);
+        const g = new Dygraph(div, d, {
+            legend: 'always',
+            title: labels[i-1],
+            showRoller: true,
+            ylabel: labels[i-1]
+        });
+        chart.appendChild(div);
+    }
+   
 }
